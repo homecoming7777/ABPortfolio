80525db1fdff20cbbb85599e86fbae7619e017f2
¨abdessamad¨ <abdobenzekri05.com>
2026-02-20 16:47:57 +0000

    fix emailjs dependency

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 export default function ContactForm() {
   useEffect(() => {
